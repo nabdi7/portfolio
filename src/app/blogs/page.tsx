f2aa741f26454fc7da7ebc37283707a562d5c124
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-08-28 17:05:20 -0700

    updated blogs

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Metadata } from "next";
+import Blogs from "@/components/blogs/Blogs";
+
+export const metadata: Metadata = {
+  title: "Najib Abdi - Blogs",
+  description: "Blog articles on software development, web development, mobile app development, science and more.",
+};
+
+const blogs = () => {
+  return (
+    <div>
+      <Blogs />
+    </div>
+  )
+}
+
+export default blogs
---
+++ src/components/blogs/Blogs.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Subscribe from '../subscribe/Subscribe';
+
+const Blogs = () => {
+ 
+  return (
+    <>
+    <section className="custom-screen">
+      <div className="flex flex-col md:flex-col max-w-3xl mx-auto ">
+        <h1 className="text-3xl font-bold">All Blogs</h1>
+      </div>
+      
+    </section>
+    <Subscribe />
+    </>
+  )
+}
+
+export default Blogs
