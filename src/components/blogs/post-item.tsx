5525766e806aab28cbb8695d2981f63ea1ace7a9
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-08-29 17:40:44 -0700

    publish

--- src/components/blogs/post-item.tsx
+++ src/components/blogs/post-item.tsx
@@ -1,7 +1,7 @@
 import { Calendar } from "lucide-react";
 import Link from "next/link";
 // import { buttonVariants } from "./ui/button";
-import { cn, formatDate } from "@/lib/utils";
+import { formatDate } from "@/lib/utils";
 import { Tag } from "./tag";
 
 interface PostItemProps {
